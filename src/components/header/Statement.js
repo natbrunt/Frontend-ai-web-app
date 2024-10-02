bd559ce01a8887a98ad1428c3c5e6298eb7ce4e5
Nathan Brunnett <natebrunnett@gmail.com>
2024-10-02 16:12:10 +0200

    Updated screen responsiveness

--- src/components/header/Statement.js
+++ src/components/header/Statement.js
@@ -13,12 +13,17 @@ function Statement() {
 
       {/* gradient statement */}
       <h1 
-        className='md:text-7xl text-5xl'>Create videos with <span className="bg-gradient-to-r from-custom-blue to-custom-red 
+        className='
+          xl:max-w-[52rem]
+          md:text-8xl sm:text-7xl text-5xl'>Create videos with <span className="bg-gradient-to-r from-custom-blue to-custom-red 
           inline-block text-transparent bg-clip-text">text prompts</span>
       </h1>
 
       {/* description */}
-      <div className='md:text-2xl text-md px-10'>
+      <div 
+        className='
+        xl:max-w-[56rem] lg:max-w-[48rem] md:max-w-[46rem]
+        md:text-2xl text-xl px-10'>
         Publish-ready videos with zero video creation skills. <span className='text-gray-400'>
         Type any topic and invideo AI creates a video with script, visuals, subtitles, voiceover & music.</span>
       </div>
@@ -33,7 +38,9 @@ function Statement() {
 
       {/* demo video */}
       <div id="video container" className='flex flex-col items-center pb-20'>
-        <video className='rounded-3xl w-4/5 shadow-[0_40px_80px_-20px_rgba(0,0,0,0.5)]' autoPlay loop muted>
+        <video className='
+        xl:min-w-[70rem]
+        rounded-3xl w-4/5 shadow-[0_40px_80px_-20px_rgba(0,0,0,0.5)]' autoPlay loop muted>
           <source src="https://cdn.pixabay.com/video/2020/04/30/37712-414754673_small.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
